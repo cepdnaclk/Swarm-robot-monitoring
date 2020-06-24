26dbab7d494fae83f500da89997c1aceb3160cd2
lashsl <lashsl05@gmail.com>
2020-06-24 21:59:20 +0530

    changed from movement detection to edge detection

--- swarm_robot_monitoring.py
+++ swarm_robot_monitoring.py
@@ -1,6 +1,9 @@
 import cv2
 
-cap = cv2.VideoCapture('../sample3.mp4')
+font = cv2.FONT_HERSHEY_SIMPLEX
+
+
+cap = cv2.VideoCapture('../sample2.mp4')
 
 if not cap.isOpened():
     print("Error")
@@ -13,23 +16,22 @@ while cap.isOpened():
         break
 
     #  detecting movements
-    diff = cv2.absdiff(frame1, frame2)
-    final = diff
-    gray = cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY)
-    blur = cv2.GaussianBlur(gray, (5, 5), 0)
-    _, thresh = cv2.threshold(blur, 20, 255, cv2.THRESH_BINARY)
-    dilated = cv2.dilate(thresh, None, iterations=3)
-    contours, _ = cv2.findContours(dilated, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    edges = cv2.Canny(frame1, 100,200)
+    contours, _ = cv2.findContours(edges, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
     coordinates = []
 
+
+
     #  drawing a rectangle around detected movements and calculating the centers of the objects
     for contour in contours:
-        if cv2.contourArea(contour) < 50 or cv2.contourArea(contour) > 1000:
+        if cv2.contourArea(contour) < 50 or cv2.contourArea(contour) > 5000:
             continue
         (x, y, w, h) = cv2.boundingRect(contour)
         coordinates = coordinates + [(int(x + w / 2), int(y + h / 2))]
         cv2.rectangle(frame1, (x, y), (x + w, y + h), (0, 255, 0), 1)
+
+    print(len(coordinates))
 
     #  drawing a line among moving objects to give a visual representation of the distance
     if len(coordinates) > 1:
@@ -37,12 +39,10 @@ while cap.isOpened():
             other = element + 1
 
             while other < len(coordinates):
-                print(element, other)
-                print(coordinates[element], coordinates[other])
                 other = other + 1
                 cv2.line(frame1, coordinates[element], coordinates[other - 1], (0, 255, 0), thickness=1, lineType=8)
 
-    cv2.imshow("frame", frame1)
+    cv2.imshow("frame", edges)
     frame1 = frame2
     ret, frame2 = cap.read()
 
