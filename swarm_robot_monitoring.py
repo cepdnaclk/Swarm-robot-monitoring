97ba5a645d8845587bb23f9c908365cd3592886b
lashsl <lashsl05@gmail.com>
2020-07-04 00:44:53 +0530

    distance calculation among robots

--- swarm_robot_monitoring.py
+++ swarm_robot_monitoring.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+import math
 
 cap = cv2.VideoCapture('../test.mp4')
 
@@ -12,7 +13,25 @@ _, frame2 = cap.read()
 lower_blue = np.array([50, 158, 124])
 upper_blue = np.array([150, 255, 255])
 
+window_name = 'Image'
+font = cv2.FONT_HERSHEY_SIMPLEX
+org = (10, 50)
+fontScale = 0.35
+color = (255, 0, 0)
+thickness = 1
+string = "Distance among robots in px : "
+
+
+def disCal(first, second):
+    lenOne = first[0] - second[0]
+    lenTwo = first[1] - second[1]
+    outPut = math.sqrt(math.pow(lenOne, 2) + math.pow(lenTwo, 2))
+    return math.floor(outPut)
+
+
 while cap.isOpened():
+    coordinates = []
+    disAmongRob = []
     if not ret:
         break
 
@@ -26,8 +45,6 @@ while cap.isOpened():
     dilated = cv2.dilate(edges, None, iterations=10)
     contours, _ = cv2.findContours(dilated, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
 
-    coordinates = []
-
     #  drawing a rectangle around detected movements and calculating the centers of the objects
     for contour in contours:
         if cv2.contourArea(contour) < 50 or cv2.contourArea(contour) > 5000:
@@ -40,17 +57,19 @@ while cap.isOpened():
     #  drawing a line among moving objects to give a visual representation of the distance
     if len(coordinates) > 1:
         for element in range(len(coordinates)):
-            other = element + 1
+            other = element+1
 
             while other < len(coordinates):
                 other = other + 1
-                cv2.line(frame1, coordinates[element], coordinates[other - 1], (0, 255, 0), thickness=1, lineType=8)
+                cv2.line(frame1, coordinates[element], coordinates[other-1], (0, 255, 0), thickness=1, lineType=8)
+                disAmongRob = disAmongRob + [disCal(coordinates[element], coordinates[other - 1])]
 
+    frame1 = cv2.putText(frame1, string + str(disAmongRob), org, font, fontScale, color, thickness, cv2.LINE_AA)
     cv2.imshow("frame", frame1)
     frame1 = frame2
     ret, frame2 = cap.read()
 
-    if cv2.waitKey(5) & 0xFF == ord('q'):
+    if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
 cv2.destroyAllWindows()
