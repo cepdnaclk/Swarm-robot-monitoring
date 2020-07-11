04bc16756c582021a87c65d69e35764e3241323f
lashsl <lashsl05@gmail.com>
2020-07-11 15:33:58 +0530

    displaying all distances on the respective line, comments modified, functions for each step, mostlikely final!

--- readme.txt
+++ readme.txt
@@ -2,15 +2,24 @@
 Instructions
 
 1. Copy all the python files.
-2. Edit the varibale "path" in main file(swarm_robot_monitoring.py) to get the location of the video file.
+
+2. Edit the variable "path" in main file(swarm_robot_monitoring.py) to get the location of the video file.
+
 3. run the main file(swarm_robot_monitoring.py).
+
 4. Video will start to play.
+
 5. Press 'c' to capture a test frame at a desired time.This will be used to set values for color filtering process.
+
 6. Then the captured frame will display. Press any key to continue.
+
 7. Change values in the interface and click "test for Robots/Arena" button to check whether selected values filter out the colour of the robots.
-8. If the color filteration is acceptable click "set for robots" to store the values for color filtering of robots.
+
+8. If the color filtration is acceptable click "set for robots" to store the values for color filtering of robots.
+     Note!!!! - in order to update values "test for Robots/Arena" must be clicked before clicking "set .. " buttons.
+
 9. Do the same to set the values to filter the color of the arena.
-*. recommended values for the sample video, provided for group B
+    *. recommended values for the sample video, provided for group B
 	For robots,
 		Channel 1 Lower limit-70
 		Channel 1 upper limit - 170
--- swarm_robot_monitoring.py
+++ swarm_robot_monitoring.py
@@ -23,24 +23,46 @@ s.setValues()
 print(s.lRob, s.uRob)
 print(s.lAr, s.uAr)
 
+# for color filtering of the robots
 lowerRobot = s.lRob
 upperRobot = s.uRob
 
+# for color filtering of the arena
 lowerArena = s.lAr
 upperArena = s.uAr
 
+# variables defining
 robotNumber = 1
+border = ()
+coordinates = []
 
 window_name = 'Image'
 font = cv2.FONT_HERSHEY_SIMPLEX
-org = (10, 50)
 fontScale = 0.35
-color = (255, 0, 0)
-thickness = 1
-string = "Distance among robots in px : "
+colorOne = (255, 0, 0)
+colorTwo = (0, 0, 255)
+stringOne = "Distance among robots in px : "
+stringTwo = "Distance from robots to left border in px : "
+stringThree = "Distance from robots to top border in px : "
 
 
-# this function use to calculate the distance between two coordinates.
+# calculating coordinates of the robots
+def coordinatesCal(con):
+    global rNum
+    global frame1
+    outPut = []
+    for contour in con:
+        if cv2.contourArea(contour) < 50 or cv2.contourArea(contour) > 5000:
+            continue
+        (x, y, w, h) = cv2.boundingRect(contour)
+        cv2.rectangle(frame1, (x, y), (x + w, y + h), (0, 255, 0), 1)
+        rNum = rNum + 1
+        frame1 = cv2.putText(frame1, "robot " + str(rNum), (x + 1, y + 1), font, fontScale, colorOne, 1, cv2.LINE_AA)
+        outPut = outPut + [(int(x + w / 2), int(y + h / 2))]
+    return outPut
+
+
+# this function calculates the distance between two coordinates.
 def disCal(first, second):
     lenOne = first[0] - second[0]
     lenTwo = first[1] - second[1]
@@ -48,11 +70,45 @@ def disCal(first, second):
     return math.floor(outPut)
 
 
+# drawing line between robots and calculating distance between
+def betweenRobs(cods):
+    global frame1
+    if len(cods) > 1:
+        for element in range(len(cods)):
+            other = element + 1
+
+            while other < len(cods):
+                other = other + 1
+                cv2.line(frame1, cods[element], cods[other - 1], (255, 0, 0), thickness=1, lineType=8)
+                distance = disCal(cods[element], cods[other - 1])
+                xOrg = int((cods[element][0] + cods[other - 1][0]) / 2)
+                yOrg = int((cods[element][1] + cods[other - 1][1]) / 2)
+                frame1 = cv2.putText(frame1, "px:" + str(distance), (xOrg, yOrg), font, 0.35, colorOne, 1, cv2.LINE_AA)
+
+
+# this function calculates the distance from left border + display the value
+def disToLeft(robot):
+    global frame1
+    dis = robot[0] - border[0]
+    org = (int((robot[0] - border[0]) / 2), robot[1])
+    frame1 = cv2.putText(frame1, "px : " + str(dis), org, font, fontScale, colorTwo, 1, cv2.LINE_AA)
+    cv2.line(frame1, robot, (border[0], robot[1]), (0, 0, 255), thickness=1, lineType=8)
+
+
+# this function calculates the distance from bottom border + displaying the value
+def disToTop(robot):
+    global frame1
+    dis = robot[1] - border[1]
+    org = (robot[0], int((robot[1] - border[0]) / 2))
+    frame1 = cv2.putText(frame1, "px : " + str(dis), org, font, fontScale, colorTwo, 1, cv2.LINE_AA)
+    cv2.line(frame1, robot, (robot[0], border[1]), (0, 0, 255), thickness=1, lineType=8)
+
+
 #  main
 while cap.isOpened():
     rNum = 0
     coordinates = []
-    disAmongRob = []
+
     if not ret:
         break
 
@@ -78,36 +134,23 @@ while cap.isOpened():
         areas = [cv2.contourArea(c) for c in contoursOfArena]
         max_index = np.argmax(areas)
         cnt = contoursOfArena[max_index]
-        (x, y, w, h) = cv2.boundingRect(cnt)
-        cv2.rectangle(frame1, (x, y), (x + w - 5, y + h - 5), (0, 0, 255), 2)
+        (xx, yy, ww, hh) = cv2.boundingRect(cnt)
+        cv2.rectangle(frame1, (xx, yy), (xx + ww - 5, yy + hh - 5), (0, 0, 255), 2)
+        border = (xx, yy)
     except ValueError:
         pass
 
-    #  drawing a rectangle around robots and calculating the centers of the objects
-    for contour in contoursOfRobot:
-        if cv2.contourArea(contour) < 50 or cv2.contourArea(contour) > 5000:
-            continue
+    #  drawing a rectangle around robots and calculating the coordinates of the robots
+    coordinates = coordinatesCal(contoursOfRobot)
 
-        rNum = rNum + 1
+    #  drawing a line among robots objects to give a visual representation of the distance and calculating
+    #  and displaying distances
+    betweenRobs(coordinates)
 
-        (x, y, w, h) = cv2.boundingRect(contour)
-        coordinates = coordinates + [(int(x + w / 2), int(y + h / 2))]
-        cv2.rectangle(frame1, (x, y), (x + w, y + h), (0, 255, 0), 1)
-        frame1 = cv2.putText(frame1, "robot " + str(rNum), (x + 1, y + 1), font, fontScale, color, thickness,
-                             cv2.LINE_AA)
+    # calculating and displaying distances to border
+    list(map(disToLeft, coordinates))
+    list(map(disToTop, coordinates))
 
-    #  drawing a line among moving objects to give a visual representation of the distance
-    if len(coordinates) > 1:
-        for element in range(len(coordinates)):
-            other = element + 1
-
-            while other < len(coordinates):
-                other = other + 1
-                cv2.line(frame1, coordinates[element], coordinates[other - 1], (0, 255, 0), thickness=1, lineType=8)
-                disAmongRob = disAmongRob + [disCal(coordinates[element], coordinates[other - 1])]
-
-    #  displaying the distances
-    frame1 = cv2.putText(frame1, string + str(disAmongRob), org, font, fontScale, color, thickness, cv2.LINE_AA)
     cv2.imshow("Swarm Robot Monitoring( Press 'q' to stop)", frame1)
     frame1 = frame2
     ret, frame2 = cap.read()
